9e867de028400bd8fafde55ac704318469174556
svytoq <niktog0@mail.ru>
2025-04-01 02:17:17 +0300

    switch configuration path

--- platform-commons/src/main/java/ru/itmo/platform/utils/configuration/LocalConfiguration.java
+++ platform-commons/src/main/java/ru/itmo/platform/utils/configuration/LocalConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Profile("ips-dev")
+@Profile("dev")
 @PropertySource("classpath:environment.properties")
 public class LocalConfiguration {
 
